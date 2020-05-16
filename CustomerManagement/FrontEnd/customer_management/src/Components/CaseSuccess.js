a05d0d398a1ea0bfe1c8b1fe65f7c13aec56ad29
MJSindhu7 <jayasindhu.manda@sjsu.edu>
2020-05-16 15:57:03 -0700

    refactoring

--- CustomerManagement/FrontEnd/customer_management/src/Components/CaseSuccess.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-class CaseSuccess extends Component {
-  render() {
-    return <div>Case created successfully</div>;
-  }
-}
-export default CaseSuccess;
