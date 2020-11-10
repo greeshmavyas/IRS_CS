3a58fd59b9d6890adfb76c74f5efcc5f5b68367a
KeerthiAkella3 <keerthi.akella@sjsu.edu>
2020-11-09 17:26:17 -0800

    caseFiltering

--- AgentManagement/frontend/src/components/Dashboard.js
+++ AgentManagement/frontend/src/components/Dashboard.js
@@ -4,23 +4,30 @@ import NavbarDash from "./NavbarDash";
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import Sidebar from './Sidebar'
 import config from '../config/settings'
-import Card from 'react-bootstrap/Card'
-import Dropdown from 'react-bootstrap/Dropdown'
 import Table from 'react-bootstrap/Table'
+import Select from 'react-select'
 
 class Dashboard extends Component {
   constructor() {
     super();
     this.state = {
-      allCases: {},
+      allCases: [],
       viewcase: null,
       modal: false,
       mstatus: "open",
-      flag: false
+      flag: false,
+      selectOptions : [],
+      status:""
+      
     };
 
   }
-
+  handleChange= (e)=> {
+    this.setState({
+      status: e.target.value
+    });
+  }
+  
   showModal = () => {
     console.log("hello");
     this.setState({
@@ -72,6 +79,16 @@ class Dashboard extends Component {
             console.log(response.data.cases)
             if (receivedCases) {
               this.setState({ allCases: receivedCases, flag: true });
+              const data = this.state.allCases
+              
+              const options = data.map(d => ({
+                "value" : d.Status,
+                "label" : d.Status
+              }))
+            this.setState({selectOptions: options})
+            console.log("options")
+            console.log(this.state.selectOptions)
+
             } else {
               this.setState({ allCases: {} });
             }
@@ -79,43 +96,52 @@ class Dashboard extends Component {
           else {
             this.setState({ allCases: {} });
           }
-  
+          
         })
         .catch(error => {
           console.log(error);
           this.setState({ allCases: {} })
         })
+        
+      
     }
     else if(count < 1){
       window.location.reload();
     }
-    
   }
 
+
   render() {
+    console.log("status");
+    console.log(this.state.status);
     const closeBtn = (
       <button className="close" onClick={() => this.showModal()}>
         &times;
        </button>
     );
     var casedetails;
+    
     if (this.state.flag) {
+      
       casedetails = this.state.allCases.map((ticket) => {
-        return (
-          <div>
-            <div onClick={() => this.showModal1(ticket)} >
-                  <Table>
-                    <tbody >
-                      <tr >
-                        <td style={{ width: '10rem' }}>{ticket.CaseID}</td>
-                        <td style={{ width: '15rem' }}>{ticket.Status}</td>
-                        <td style={{ width: '50rem' }}>{ticket.Information} </td>
-                      </tr>
-                    </tbody>
-                  </Table>            
+        if(this.state.status == ticket.Status || this.state.status == ""){
+          return (
+            <div>
+              <div onClick={() => this.showModal1(ticket)} >
+                    <Table>
+                      <tbody >
+                        <tr >
+                          <td style={{ width: '10rem' }}>{ticket.CaseID}</td>
+                          <td style={{ width: '15rem' }}>{ticket.Status}</td>
+                          <td style={{ width: '50rem' }}>{ticket.Information} </td>
+                        </tr>
+                      </tbody>
+                    </Table>            
+              </div>
             </div>
-          </div>
-        );
+          );
+        }
+        
       });
     }
     return (
@@ -126,18 +152,24 @@ class Dashboard extends Component {
             <Sidebar />
           </div>
           <div className="container col-9">  
-          <form action="/action_page.php">
-            <label for="filters">Filter by case status:   </label>
-              <select name="status" id="status">
-                <option value="New">New</option>
-                <option value="Open">Open</option>
-                <option value="Resolved">Resolved</option>
-                <option value="Pending">Pending</option>
-              </select>
-            <br></br>
-            <br></br>
-            <br></br>
-          </form>
+         
+          <div className="filter-item">
+            {this.state.allCases.map(function(item) {
+            return ( 
+              <div>{item.name}</div>
+            );
+            })}
+          </div>
+          <div>
+            Filter by Status:  < nbsp />
+            <select id="Status" value= {this.state.status} onChange={this.handleChange}>
+              <option value="">All Cases</option>
+              <option value="Assigned">Assigned</option>
+              <option value="InProgress">In Progress</option>
+              <option value="Resolved">Resolved</option> 
+            </select> 
+            </div>
+          
           <Table>
           <tr>
             <th style={{ width: '10rem' }}>ID</th>
@@ -145,6 +177,7 @@ class Dashboard extends Component {
             <th style={{ width: '50rem' }}>Details</th>
           </tr>
           </Table>
+
           {this.state.allCases.length > 0 ? (
               <div className="col-10">
                 {casedetails}
@@ -166,23 +199,12 @@ class Dashboard extends Component {
                         <p className="font-weight col-7">
                           Information : {this.state.viewcase.Information}
                         </p>
-                        <div className="col-3">
-                          <button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {this.state.mstatus}</button>
-                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <button onClick={() => this.handleStatusChange("Working")} class="dropdown-item" >Working</button>
-                            <button onClick={() => this.handleStatusChange("Resolved")} class="dropdown-item" >Resolved</button>
-                            <button onClick={() => this.handleStatusChange("Postponed")} class="dropdown-item" >Postponed</button>
-                          </div>
-                        </div>
                       </div>
                       <div className="form-group">
                         <p className="font-weight-bold">
                           Resolution Comments : {this.state.viewcase.ResolutionComments}
                         </p>
-
                       </div>
-
                     </ModalBody>
                     <ModalFooter>
                     </ModalFooter>
@@ -195,7 +217,6 @@ class Dashboard extends Component {
                 </div>
               )}
           </div>
-
         </div>
       </div>
     );
