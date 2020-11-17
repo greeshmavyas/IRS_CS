38c7e5e68017684555a9d695bf6dc1e8fb014098
Greeshma Vyas <greeshmavyas06@gmail.com>
2020-11-17 02:05:17 -0800

    Agent update API and postman request

--- OrgOwner/Backend/src/routes/updateByOrgOwner.js
+++ OrgOwner/Backend/src/routes/updateByOrgOwner.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 let Organization = require("../models/OrganizationModel");
+let Agent = require("../models/AgentModel");
 var kafka = require("../routes/kafka/client");
 
 // Add a new category to the organization
@@ -13,7 +14,7 @@ router.route("/org/category").put(function (req, res) {
 
   Organization.findOneAndUpdate(
     { _id: req.body.OrgID },
-    { $addToSet: { Categories: req.body.Category } },
+    { $addToSet: { Categories: req.body.Category }},
     { new: true },
     function (err, org) {
       if (err) {
@@ -30,5 +31,46 @@ router.route("/org/category").put(function (req, res) {
     }
   );
 });
+
+//Update agent categories[only add new categories], email, first name, last name
+//categories to be sent as combination of orgid and category
+router.route("/org/agent").put(function (req, res) {
+  //Create kafka topic corresponding to org category
+  kafka.setTopics(req.body.Categories);
+
+  Agent.findOneAndUpdate(
+    { Username: req.body.Username },
+    { $addToSet: { Categories: req.body.Categories },  $set:{ FirstName: req.body.FirstName,
+      LastName: req.body.LastName,
+      Email: req.body.Email}},
+    { new: true },
+    function (err, agent) {
+      if (err) {
+        console.log(err);
+        console.log("unable to update database");
+        res.status(400).json({ responseMessage: "unable to update database" });
+      } else {
+        for (i = 0; i < req.body.Categories.length; i++) {
+          if (global.topics_to_agents.has(req.body.Categories[i])) {
+            global.topics_to_agents
+              .get(req.body.Categories[i])
+              .push(agent._id);
+          } else {
+            global.topics_to_agents.set(req.body.Categories[i], [
+              agent._id,
+            ]);
+          }
+        }
+        console.log("result:", agent);
+        console.log("Agent update successful");
+        console.log("topics" + JSON.stringify(global.topics_to_agents));
+        res
+          .status(200)
+          .json({ responseMessage: "Agent update successful", agent });
+      }
+    }
+  );
+});
+
 
 module.exports = router;
