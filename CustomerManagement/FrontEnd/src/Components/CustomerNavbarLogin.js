1273f8955c54be87ca0da7bdc372b4bfd25bb8ed
Greeshma Vyas <greeshmavyas06@gmail.com>
2020-11-23 17:58:00 -0800

    Adding customer management to the rest of master code

---
+++ CustomerManagement/FrontEnd/src/Components/CustomerNavbarLogin.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class CustomerNavbarLogin extends Component {
+    
+  handleLogout = () => {
+    window.localStorage.clear();
+  };
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="/companyDashboard">
+            Home Org 
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="/customerDashboard">
+                  Customer Service
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/" onClick={this.handleLogout}>
+                  Help
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default CustomerNavbarLogin;
