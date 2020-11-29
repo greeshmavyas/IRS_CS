12965d8308c07711bfa849a602bbbd5db2c77003
anjalibandaru2 <bandaruanjali@gmail.com>
2020-11-29 14:37:15 -0800

    modified ports

--- AgentManagement/frontend/src/config/settings.js
+++ AgentManagement/frontend/src/config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "hostname": "ec2-54-67-107-187.us-west-1.compute.amazonaws.com",
+    "hostname": "ec2-52-53-217-170.us-west-1.compute.amazonaws.com",
     "backendPort": 4001,
-    'frontendHostName': 'ec2-54-67-107-187.us-west-1.compute.amazonaws.com',
+    'frontendHostName': 'ec2-52-53-217-170.us-west-1.compute.amazonaws.com',
     'frontendPort': 3001,
-    "rooturl" : "http://ec2-54-67-107-187.us-west-1.compute.amazonaws.com:4001",
+    "rooturl" : "http://ec2-52-53-217-170.us-west-1.compute.amazonaws.com:4001",
 };
